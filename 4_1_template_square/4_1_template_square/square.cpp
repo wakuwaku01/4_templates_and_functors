d69a6a2c18ddeab9a71f6566ed2d39d89b2c1c83
wakuwaku01 <wakuwaku.post.mail@gmail.com>
2024-03-21 20:06:27 +0300

    change tasks 1 and 2

--- 4_1_template_square/4_1_template_square/square.cpp
+++ 4_1_template_square/4_1_template_square/square.cpp
@@ -5,39 +5,48 @@
 #include <vector>
 
 template <class T>
-T square(T num)
+T square(T num) 
 {
 	return num * num;
 }
 
 template <class T>
-void square(std::vector<T>& vec)
+std::vector<T> square(std::vector<T> vec) 
 {
-	for (int i = 0; i < vec.size(); ++i)
+	for (int i = 0; i < vec.size(); ++i) 
 	{
 		vec[i] *= vec[i];
 	}
+	return vec;
 }
 
-int main()
+template <class T>
+void print(std::vector<T> vec) 
 {
+	std::for_each(vec.begin(), vec.end(), [](int num) 
+		{
+		std::cout << num << " ";
+		});
+}
+
+int main() {
 	int num = 4;
 	std::vector<int> vec = { -1, 4, 8 };
 
 	std::cout << "[IN]: " << num << std::endl;
 	std::cout << "[OUT]: " << square(num) << std::endl;
+	std::cout << num << std::endl;
 	std::cout << "[IN]: ";
-	std::for_each(vec.begin(), vec.end(), [](int num)
-		{
-			std::cout << num << " ";
-		});
+	print(vec);
+//	std::for_each(vec.begin(), vec.end(), [](int num) {	std::cout << num << " "; });
 
 	std::cout << std::endl;
 
 	std::cout << "[OUT]: ";
-	square(vec);
-	std::for_each(vec.begin(), vec.end(), [](int num)
-		{
-			std::cout << num << " ";
-		});
+	print(square(vec));
+
+//	std::for_each(vec.begin(), vec.end(), [square(vec)](int num) {	std::cout << num << " "; });
+
+//	for (int i = 0; i < vec.size(); ++i) { std::cout << vec[i] << " "; }
+
 }
--- 4_2_template_table/4_2_template_table/table.cpp
+++ 4_2_template_table/4_2_template_table/table.cpp
@@ -10,29 +10,47 @@ private:
     T** table = nullptr;
 
 public:
-    Table(int rows_count, int columns_count) : rows(rows_count), columns(columns_count) {
+ 
+    Table (const Table& rhs) { operator=(rhs); }
+ 
+    Table& operator=(const Table& rhs)
+    {
+        if (this == &rhs) return *this;
+        return *this;
+    }
+
+    //Table(const Table&) = delete; //конструктор копирования
+    //Table & operator= (const Table&) = delete; //оператор присваивания
+
+    Table(int rows_count, int columns_count) : rows(rows_count), columns(columns_count) 
+    {
         table = new T * [rows];
-        for (int i = 0; i < rows; i++) {
+        for (int i = 0; i < rows; i++) 
+        {
             table[i] = new T[columns];
         }
     }
 
     ~Table() {
-        for (int i = 0; i < rows; i++) {
-            delete table[i];
+        for (int i = 0; i < rows; i++) 
+        {
+            delete[] table[i];
         }
         delete[] table;
     }
 
-    T* operator[](int i) {
+    T* operator[](int i) 
+    {
         return table[i];
     }
 
-    T* operator[](const int i) const {
+    T* operator[](const int i) const 
+    {
         return table[i];
     }
 
-    int size() const {
+    int size() const 
+    {
         return rows * columns;
     }
 };
@@ -46,7 +64,16 @@ int main()
     std::cout << test[0][0] << std::endl;
     std::cout << "Size: " << test.size() << std::endl;
 
+    {
+        Table<int> t1(2, 3);
+        Table<int> t2(2, 3);
+        t1 = t2;
+    } // Программа упадет здесь?
 
+    {
+        Table<double> t1(2, 3);
+        Table<double> t3(t1);
+    }
 
     return 0;
 }
